c7c1bc9bd19629f5d8f2d2fed2feae578be5742e
KIPROTYCH <kiprotych@gmail.com>
2023-08-11 14:07:07 +0300

    Modified the Header function

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,25 @@
 import { useNavigate } from "react-router-dom";
+import './Header.css'
 
 const headerElements = [
-  { label: 'TESLA', route: '/' },
+  { label: 'TESLA', route: '', style:{marginRight:'auto'} },
   { label: 'Model S', route: 'model_s' },
   { label: 'Model X', route: 'model_x' },
-  { label: 'CYBERTRUCK', route: 'cybertruck' },
-  { label: 'Menu', route: 'null' }
+  { label: 'CyberTruck', route: 'cybertruck' },
+  { label: 'Menu', route: 'null', style: {marginLeft: 'auto'} }
 ];
 
 export default function Header () {
     const navigate = useNavigate()
     return (
       <nav>
-        <ul>
-          {headerElements.map(({ label, route }) => (
-            <li key={label} onClick={() => {
-            if (!label === 'Menu') {navigate(route);
+        <ul className="headerList">
+          {headerElements.map(({ label, route, style={} }) => (
+            <li
+            style={style}
+             key={label} onClick={() => {
+            if (!(label === 'Menu')) 
+            {navigate(`/${route}`);
             }
             }}
             >{label}</li>
