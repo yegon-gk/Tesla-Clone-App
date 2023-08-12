3a4462f12cdeff44b40dafd9cb2bc37678602175
KIPROTYCH <kiprotych@gmail.com>
2023-08-12 20:06:32 +0300

    Modified these files

---
+++ src/components/Advert/Advert.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Advert.css';
+
+export default function Advert() {
+  return (
+    <div className="advertisement-banner">
+      <div className="advertisement-content">
+        <h1>Advert Banner</h1>
+        {/* Additional content for advertisement */}
+      </div>
+    </div>
+  );
+}
